38213dc8d7b05a0741c33a019c9aa4fe431a0ea8
Chris Dillon <chrisdillon.us@gmail.com>
2023-02-04 05:53:05 -0500

    Create functions for filter list and list items

--- filters.php
+++ filters.php
@@ -12,6 +12,7 @@ function wpshortlist_filters() {
 
 	// Get current URL without args (the CPT/CT permalink only).
 	$current_url = wpshortlist_get_current_url();
+	// phpcs:ignore
 	// q2($current_url,__FUNCTION__.': current_url');
 
 	// Get query args (our post meta).
@@ -19,6 +20,7 @@ function wpshortlist_filters() {
 
 	// Remove CPT/CT.
 	unset( $current_args['feature'] );
+	// phpcs:ignore
 	// q2($current_args, __FUNCTION__.': current_args');
 
 	// Filter sets.
@@ -56,4 +58,62 @@ function wpshortlist_get_current_query_args() {
 	*/
 	global $wp_query;
 	return $wp_query->query;
+}
+
+/**
+ * Print filter list.
+ *
+ * @param Array $filter  A filter.
+ *
+ * @return void
+ */
+function wpshortlist_print_filter_list( $filter ) {
+	?>
+	<ul class="wpshortlist-filter-list">
+	<?php
+	foreach ( $filter['options'] as $option_id => $option_name ) :
+
+		// Build a unique ID like 'supports-display-term-list-tags'.
+		$input_id = $filter['query_var'] . '-' . $option_id;
+
+		$checked = isset( $current_args[ $filter['query_var'] ] )
+			&& $option_id === $current_args[ $filter['query_var'] ];
+
+		$args = array(
+			'id'      => $input_id,
+			'name'    => $filter['id'],
+			'value'   => $option_id,
+			'title'   => $option_id,
+			'label'   => $option_name,
+			'checked' => $checked,
+		);
+		wpshortlist_filter_list_item( $args );
+		?>
+
+	<?php endforeach; ?>
+	</ul>
+	<?php
+}
+
+/**
+ * Print filter list item.
+ *
+ * @param Array $args  HTML input attributes.
+ *
+ * @return void
+ */
+function wpshortlist_filter_list_item( $args ) {
+	?>
+	<li class="wpshortlist-filter-list-item">
+		<input type="radio"
+			id="<?php echo esc_attr( $args['id'] ); ?>"
+			name="<?php echo esc_attr( $args['name'] ); ?>"
+			value="<?php echo esc_attr( $args['value'] ); ?>"
+			title="<?php echo esc_attr( $args['title'] ); ?>"
+			<?php checked( $args['checked'] ); ?> />
+		<label for="<?php echo esc_attr( $args['id'] ); ?>">
+			<?php echo esc_html( $args['label'] ); ?>
+		</label>
+	</li>
+	<?php
 }
--- template-parts/default.php
+++ template-parts/default.php
@@ -30,17 +30,17 @@ HTML Structure:
 ?>
 <form id="wpshortlist-form" class="wpshortlist-form">
 	<?php
+	// @todo Replace this with getter(tax, term).
 	foreach ( $config as $filter_set ) :
 		// Is this filter set for this term?
 		if ( ! is_tax( $filter_set['taxonomy'], $filter_set['term'] ) ) {
 			continue;
 		}
 		?>
+
 	<div class="wpshortlist-filterset">
-		<?php
-		// Print filter.
-		foreach ( $filter_set['filters'] as $filter ) :
-			?>
+		<?php foreach ( $filter_set['filters'] as $filter ) : ?>
+
 		<div class="wpshortlist-filter">
 			<fieldset>
 				<legend>
@@ -48,34 +48,12 @@ HTML Structure:
 						<?php echo esc_html( $filter['name'] ); ?>
 					</h3>
 				</legend>
-				<ul class="wpshortlist-filter-list">
-					<?php
-					foreach ( $filter['options'] as $option_id => $option_name ) :
-
-						// Build a unique ID like 'supports-display-term-list-tags'.
-						$unique_id = $filter['query_var'] . '-' . $option_id;
-
-						$checked = isset( $current_args[ $filter['query_var'] ] )
-							&& $option_id === $current_args[ $filter['query_var'] ];
-
-						?>
-					<li class="wpshortlist-filter-list-item">
-						<input type="radio"
-								id="<?php echo esc_attr( $unique_id ); ?>"
-								name="<?php echo esc_attr( $filter['id'] ); ?>"
-								value="<?php echo esc_attr( $option_id ); ?>"
-								title="<?php echo esc_attr( $option_id ); ?>"
-								<?php checked( $checked ); ?> />
-						<label for="<?php echo esc_attr( $unique_id ); ?>">
-							<?php echo esc_html( $option_name ); ?>
-						</label>
-					</li>
-					
-					<?php endforeach; /* filter options */ ?>
-				</ul>
+				<?php wpshortlist_print_filter_list( $filter ); ?>
 			</fieldset>
 		</div><!-- .wpshortlist-filter -->
+
 		<?php endforeach; ?>
 	</div><!-- .wpshortlist-filterset -->
+
 	<?php endforeach; ?>
 </form>
