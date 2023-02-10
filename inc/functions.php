e82450dd4c82f89b4ca8d1bddd5e28fb1adccaad
Chris Dillon <chrisdillon.us@gmail.com>
2023-02-09 19:19:26 -0500

    Remove unused function

--- inc/functions.php
+++ inc/functions.php
@@ -6,30 +6,6 @@
  */
 
 /**
- * Get all post meta values for a specific key.
- */
-function wpshortlist_get_all_post_meta() {
-	global $wpdb;
-
-	$sql = "SELECT DISTINCT `meta_value` FROM `wp_postmeta`
-			WHERE `meta_key` = 'method-display-terms-current-post'
-			ORDER BY 1 ASC";
-
-	if ( $wpdb->last_error ) {
-		return 'wpdb error: ' . $wpdb->last_error;
-	}
-
-	$results = $wpdb->get_results( $sql, ARRAY_A );
-
-	foreach ( $results as $result ) {
-		$meta_values[] = $result['meta_value'];
-	}
-	// q2($meta_values,'meta_values');
-
-	return $meta_values;
-}
-
-/**
  * Use default featured image if Tool has no image.
  *
  * @param int     $thumbnail_id  Thumbnail ID.
