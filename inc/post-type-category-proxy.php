a71fab5852464662e1722bbaa0d5602c9801df7a
Chris Dillon <chrisdillon.us@gmail.com>
2023-02-10 17:53:13 -0500

    Formatting

--- inc/post-type-category-proxy.php
+++ inc/post-type-category-proxy.php
@@ -1,100 +1,87 @@
 <?php
-
 /**
  * Post Type: Category Proxy
+ *
+ * @package wpshortlist
+ */
+
+/**
+ * Register post type.
  */
 function wpshortlist_register_post_type__category_proxy() {
 
-	$labels = [
-		'name' => esc_html__( 'Categories Proxy', 'wpshortlist' ),
-		'singular_name' => esc_html__( 'Category Proxy', 'wpshortlist' ),
-		'menu_name' => esc_html__( 'Categories (proxy)', 'wpshortlist' ),
-		'all_items' => esc_html__( 'All Categories Proxy', 'wpshortlist' ),
-		'add_new' => esc_html__( 'Add new', 'wpshortlist' ),
-		'add_new_item' => esc_html__( 'Add new Category Proxy', 'wpshortlist' ),
-		'edit_item' => esc_html__( 'Edit Category Proxy', 'wpshortlist' ),
-		'new_item' => esc_html__( 'New Category Proxy', 'wpshortlist' ),
-		'view_item' => esc_html__( 'View Category Proxy', 'wpshortlist' ),
-		'view_items' => esc_html__( 'View Categories Proxy', 'wpshortlist' ),
-		'search_items' => esc_html__( 'Search Categories Proxy', 'wpshortlist' ),
-		'not_found' => esc_html__( 'No Categories Proxy found', 'wpshortlist' ),
-		'not_found_in_trash' => esc_html__( 'No Categories Proxy found in trash', 'wpshortlist' ),
-		'parent' => esc_html__( 'Parent Category Proxy:', 'wpshortlist' ),
-		'featured_image' => esc_html__( 'Featured image for this Category Proxy', 'wpshortlist' ),
-		'set_featured_image' => esc_html__( 'Set featured image for this Category Proxy', 'wpshortlist' ),
-		'remove_featured_image' => esc_html__( 'Remove featured image for this Category Proxy', 'wpshortlist' ),
-		'use_featured_image' => esc_html__( 'Use as featured image for this Category Proxy', 'wpshortlist' ),
-		'archives' => esc_html__( 'Category Proxy archives', 'wpshortlist' ),
-		'insert_into_item' => esc_html__( 'Insert into Category Proxy', 'wpshortlist' ),
-		'uploaded_to_this_item' => esc_html__( 'Upload to this Category Proxy', 'wpshortlist' ),
-		'filter_items_list' => esc_html__( 'Filter Categories Proxy list', 'wpshortlist' ),
-		'items_list_navigation' => esc_html__( 'Categories Proxy list navigation', 'wpshortlist' ),
-		'items_list' => esc_html__( 'Categories Proxy list', 'wpshortlist' ),
-		'attributes' => esc_html__( 'Categories Proxy attributes', 'wpshortlist' ),
-		'name_admin_bar' => esc_html__( 'Category Proxy', 'wpshortlist' ),
-		'item_published' => esc_html__( 'Category Proxy published', 'wpshortlist' ),
+	$labels = array(
+		'name'                     => esc_html__( 'Categories Proxy', 'wpshortlist' ),
+		'singular_name'            => esc_html__( 'Category Proxy', 'wpshortlist' ),
+		'menu_name'                => esc_html__( 'Categories (proxy)', 'wpshortlist' ),
+		'all_items'                => esc_html__( 'All Categories Proxy', 'wpshortlist' ),
+		'add_new'                  => esc_html__( 'Add new', 'wpshortlist' ),
+		'add_new_item'             => esc_html__( 'Add new Category Proxy', 'wpshortlist' ),
+		'edit_item'                => esc_html__( 'Edit Category Proxy', 'wpshortlist' ),
+		'new_item'                 => esc_html__( 'New Category Proxy', 'wpshortlist' ),
+		'view_item'                => esc_html__( 'View Category Proxy', 'wpshortlist' ),
+		'view_items'               => esc_html__( 'View Categories Proxy', 'wpshortlist' ),
+		'search_items'             => esc_html__( 'Search Categories Proxy', 'wpshortlist' ),
+		'not_found'                => esc_html__( 'No Categories Proxy found', 'wpshortlist' ),
+		'not_found_in_trash'       => esc_html__( 'No Categories Proxy found in trash', 'wpshortlist' ),
+		'parent'                   => esc_html__( 'Parent Category Proxy:', 'wpshortlist' ),
+		'featured_image'           => esc_html__( 'Featured image for this Category Proxy', 'wpshortlist' ),
+		'set_featured_image'       => esc_html__( 'Set featured image for this Category Proxy', 'wpshortlist' ),
+		'remove_featured_image'    => esc_html__( 'Remove featured image for this Category Proxy', 'wpshortlist' ),
+		'use_featured_image'       => esc_html__( 'Use as featured image for this Category Proxy', 'wpshortlist' ),
+		'archives'                 => esc_html__( 'Category Proxy archives', 'wpshortlist' ),
+		'insert_into_item'         => esc_html__( 'Insert into Category Proxy', 'wpshortlist' ),
+		'uploaded_to_this_item'    => esc_html__( 'Upload to this Category Proxy', 'wpshortlist' ),
+		'filter_items_list'        => esc_html__( 'Filter Categories Proxy list', 'wpshortlist' ),
+		'items_list_navigation'    => esc_html__( 'Categories Proxy list navigation', 'wpshortlist' ),
+		'items_list'               => esc_html__( 'Categories Proxy list', 'wpshortlist' ),
+		'attributes'               => esc_html__( 'Categories Proxy attributes', 'wpshortlist' ),
+		'name_admin_bar'           => esc_html__( 'Category Proxy', 'wpshortlist' ),
+		'item_published'           => esc_html__( 'Category Proxy published', 'wpshortlist' ),
 		'item_published_privately' => esc_html__( 'Category Proxy published privately.', 'wpshortlist' ),
-		'item_reverted_to_draft' => esc_html__( 'Category Proxy reverted to draft.', 'wpshortlist' ),
-		'item_scheduled' => esc_html__( 'Category Proxy scheduled', 'wpshortlist' ),
-		'item_updated' => esc_html__( 'Category Proxy updated.', 'wpshortlist' ),
-		'parent_item_colon' => esc_html__( 'Parent Category Proxy:', 'wpshortlist' ),
-	];
+		'item_reverted_to_draft'   => esc_html__( 'Category Proxy reverted to draft.', 'wpshortlist' ),
+		'item_scheduled'           => esc_html__( 'Category Proxy scheduled', 'wpshortlist' ),
+		'item_updated'             => esc_html__( 'Category Proxy updated.', 'wpshortlist' ),
+		'parent_item_colon'        => esc_html__( 'Parent Category Proxy:', 'wpshortlist' ),
+	);
 
-	$args = [
-		'label' => esc_html__( 'Categories', 'wpshortlist' ),
-		'labels' => $labels,
-		'description' => '',
+	$args = array(
+		'labels'                => $labels,
+		'description'           => '',
 
-		'public' => true,
-		/*
-		 * inherited from $public:
-		 */
-		'publicly_queryable' => true,
-		'show_ui' => true,
-		'show_in_nav_menus' => true,
-		/*
-		 * then inherited from $show_ui:
-		 */ 
-		'show_in_menu' => true,
-		/*
-		 * then inherited from $show_in_menu:
-		 */
-		'show_in_admin_bar' => false,
+		'public'                => true,
+		'publicly_queryable'    => true,
+		'show_ui'               => true,
+		'show_in_nav_menus'     => true,
+		'show_in_menu'          => true,
+		'show_in_admin_bar'     => false,
+		'exclude_from_search'   => false,
+		'hierarchical'          => false,
 
-		'exclude_from_search' => false,
-
-		'hierarchical' => false,
-
-		/*
-		 * Don't comment out in case something enables the classic editor.
-		 */
-		'show_in_rest' => true,
-		'rest_base' => '',
+		'show_in_rest'          => true,
+		'rest_base'             => '',
 		'rest_controller_class' => 'WP_REST_Posts_Controller',
-		'rest_namespace' => 'wp/v2',
+		'rest_namespace'        => 'wp/v2',
 
-		'menu_position' => null,
-		'menu_icon' => null,   // then 'dashicons-admin-post'
+		'menu_position'         => null,
+		'menu_icon'             => null,
+		'capability_type'       => 'post',
+		'map_meta_cap'          => true,
+		'can_export'            => false,
+		'delete_with_user'      => false,
 
-		'capability_type' => 'post',
-		'map_meta_cap' => true,
+		'has_archive'           => true,
+		'rewrite'               => array(
+			'slug'       => 'category_proxy',
+			'with_front' => true,
+		),
+		'query_var'             => true,
 
-		// 'has_archive' => 'wp-categories',
-		'has_archive' => true,
-		'rewrite' => [ 'slug' => 'category_proxy', 'with_front' => true ],
-		'query_var' => true,
-		
-		'can_export' => false,
-		'delete_with_user' => false,
+		'template'              => array(),
+		'supports'              => array( 'title', 'editor', 'thumbnail' ),
+		'taxonomies'            => array(),
+	);
 
-		'template' => [],
-		'supports' => [ 'title', 'editor', 'thumbnail' ],
-		'taxonomies' => [],
-
-		// 3rd party
-		'show_in_graphql' => false,
-	];
-
-	// key must not exceed 20 characters
+	// Key must not exceed 20 characters.
 	register_post_type( 'category_proxy', $args );
 }
--- inc/taxonomy-tool-type.php
+++ inc/taxonomy-tool-type.php
@@ -1,94 +1,63 @@
 <?php
 /**
  * Taxonomy: Tool Type
- * 
- * No archive.
+ *
+ * @package wpshirtlist
+ */
+
+/**
+ * Register taxonomy.
  */
 function wpshortlist_register_taxonomy__tool_type() {
 
-	$labels = [
-		'name' => esc_html__( 'Tool Types', 'wpshortlist' ),
-		'singular_name' => esc_html__( 'Tool Type', 'wpshortlist' ),
-		'menu_name' => esc_html__( 'Tool Types', 'wpshortlist' ),
-		'all_items' => esc_html__( 'All Tool Types', 'wpshortlist' ),
-		'edit_item' => esc_html__( 'Edit Tool Type', 'wpshortlist' ),
-		'view_item' => esc_html__( 'View Tool Type', 'wpshortlist' ),
-		'update_item' => esc_html__( 'Update Tool Type name', 'wpshortlist' ),
-		'add_new_item' => esc_html__( 'Add new Tool Type', 'wpshortlist' ),
-		'new_item_name' => esc_html__( 'New Tool Type name', 'wpshortlist' ),
-		'parent_item' => esc_html__( 'Parent Tool Type', 'wpshortlist' ),
-		'parent_item_colon' => esc_html__( 'Parent Tool Type:', 'wpshortlist' ),
-		'search_items' => esc_html__( 'Search Tool Types', 'wpshortlist' ),
-		'popular_items' => esc_html__( 'Popular Tool Types', 'wpshortlist' ),
+	$labels = array(
+		'name'                       => esc_html__( 'Tool Types', 'wpshortlist' ),
+		'singular_name'              => esc_html__( 'Tool Type', 'wpshortlist' ),
+		'menu_name'                  => esc_html__( 'Tool Types', 'wpshortlist' ),
+		'all_items'                  => esc_html__( 'All Tool Types', 'wpshortlist' ),
+		'edit_item'                  => esc_html__( 'Edit Tool Type', 'wpshortlist' ),
+		'view_item'                  => esc_html__( 'View Tool Type', 'wpshortlist' ),
+		'update_item'                => esc_html__( 'Update Tool Type name', 'wpshortlist' ),
+		'add_new_item'               => esc_html__( 'Add new Tool Type', 'wpshortlist' ),
+		'new_item_name'              => esc_html__( 'New Tool Type name', 'wpshortlist' ),
+		'parent_item'                => esc_html__( 'Parent Tool Type', 'wpshortlist' ),
+		'parent_item_colon'          => esc_html__( 'Parent Tool Type:', 'wpshortlist' ),
+		'search_items'               => esc_html__( 'Search Tool Types', 'wpshortlist' ),
+		'popular_items'              => esc_html__( 'Popular Tool Types', 'wpshortlist' ),
 		'separate_items_with_commas' => esc_html__( 'Separate Tool Types with commas', 'wpshortlist' ),
-		'add_or_remove_items' => esc_html__( 'Add or remove Tool Types', 'wpshortlist' ),
-		'choose_from_most_used' => esc_html__( 'Choose from the most used Tool Types', 'wpshortlist' ),
-		'not_found' => esc_html__( 'No Tool Types found', 'wpshortlist' ),
-		'no_terms' => esc_html__( 'No Tool Types', 'wpshortlist' ),
-		'items_list_navigation' => esc_html__( 'Tool Types list navigation', 'wpshortlist' ),
-		'items_list' => esc_html__( 'Tool Types list', 'wpshortlist' ),
-		'back_to_items' => esc_html__( 'Back to Tool Types', 'wpshortlist' ),
-		'name_field_description' => esc_html__( 'The name is how it appears on your site.', 'wpshortlist' ),
-		'parent_field_description' => esc_html__( 'Assign a parent term to create a hierarchy. The term Jazz, for example, would be the parent of Bebop and Big Band.', 'wpshortlist' ),
-		'slug_field_description' => esc_html__( 'The slug is the URL-friendly version of the name. It is usually all lowercase and contains only letters, numbers, and hyphens.', 'wpshortlist' ),
-		'desc_field_description' => esc_html__( 'The description is not prominent by default; however, some themes may show it.', 'wpshortlist' ),
-	];
-	
-	$args = [
-		// 'label' => esc_html__( 'Tool Types', 'wpshortlist' ),
-		'labels' => $labels,
+		'add_or_remove_items'        => esc_html__( 'Add or remove Tool Types', 'wpshortlist' ),
+		'choose_from_most_used'      => esc_html__( 'Choose from the most used Tool Types', 'wpshortlist' ),
+		'not_found'                  => esc_html__( 'No Tool Types found', 'wpshortlist' ),
+		'no_terms'                   => esc_html__( 'No Tool Types', 'wpshortlist' ),
+		'items_list_navigation'      => esc_html__( 'Tool Types list navigation', 'wpshortlist' ),
+		'items_list'                 => esc_html__( 'Tool Types list', 'wpshortlist' ),
+		'back_to_items'              => esc_html__( 'Back to Tool Types', 'wpshortlist' ),
+		'name_field_description'     => esc_html__( 'The name is how it appears on your site.', 'wpshortlist' ),
+		'parent_field_description'   => esc_html__( 'Assign a parent term to create a hierarchy.', 'wpshortlist' ),
+		'slug_field_description'     => esc_html__( 'The slug is the URL-friendly version of the name. It is usually all lowercase and contains only letters, numbers, and hyphens.', 'wpshortlist' ),
+		'desc_field_description'     => esc_html__( 'The description is not prominent by default; however, some themes may show it.', 'wpshortlist' ),
+	);
 
-		/* 
-		 * To enable front-end archive:
-		 *   'public' => true,
-		 *   'publicly_queryable' => true,
-		 *   'show_ui' => true,   // default = $public
-		 *   'rewrite' => true,   // or custom; recommended but optional
-		 *
-		 * To disable front-end archive:
-		 *   'public' => false,
-		 *   'publicly_queryable' => false,
-		 *   'show_ui' => true,   // default = $public -> so must be explicit
-		 *   'show_tagcloud' => false,
-		 *   'rewrite' => false,
-		 */
+	$args = array(
+		'labels'                => $labels,
+		'public'                => false,
+		'publicly_queryable'    => false,
+		'show_in_nav_menus'     => true,
+		'show_ui'               => true,
+		'show_tagcloud'         => false,
+		'show_in_quick_edit'    => true,
+		'show_in_menu'          => true,
+		'show_admin_column'     => true,
+		'hierarchical'          => true,
 
-		// 'public' => true,   // required if using Proxy
-		'public' => false,
-		/*
-		 * inherited from $public: 
-		 */
-		// 'publicly_queryable' => true,
-		'publicly_queryable' => false,
-		'show_in_nav_menus' => true,   // false if using proxy
-		'show_ui' => true,
-		/* 
-		 * then inherited from $show_ui: 
-		 */
-		'show_tagcloud' => false,
-		// 'show_in_quick_edit' => true,
-		// 'show_in_menu' => true,
-		
-		'hierarchical' => true,
+		'rewrite'               => false,
+		'query_var'             => true,
 
-		/*
-		 * if using proxy, `rewrite` and `query_var` recommended but not required
-		 */
-		// 'rewrite' => true,
-		// 'rewrite' => [ 'slug' => 'types', 'with_front' => true, ],
-		'rewrite' => false,
-		'query_var' => true,
-
-		'show_admin_column' => true,
-
-		'show_in_rest' => true,
-		'rest_base' => 'tool_type',
+		'show_in_rest'          => true,
+		'rest_base'             => 'tool_type',
 		'rest_controller_class' => 'WP_REST_Terms_Controller',
-		'rest_namespace' => 'wp/v2',
+		'rest_namespace'        => 'wp/v2',
+	);
 
-		// custom
-		// 'path' => '',
-	];
-
-	register_taxonomy( 'tool_type', [ 'tool' ], $args );
+	register_taxonomy( 'tool_type', array( 'tool' ), $args );
 }
