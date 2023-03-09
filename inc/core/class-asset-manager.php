338abf403f2120a72851028ec7cb9308e7aa33cd
Chris Dillon <chrisdillon.us@gmail.com>
2023-03-09 05:15:14 -0500

    Separate the loading of scripts and styles, and disable script

--- inc/core/class-asset-manager.php
+++ inc/core/class-asset-manager.php
@@ -23,11 +23,12 @@ class Asset_Manager {
 	 * Init
 	 */
 	public function init() {
-		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+		// add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ) );
 	}
 
 	/**
-	 * Load scripts and styles
+	 * Load scripts.
 	 */
 	public function enqueue_scripts() {
 		// Only load if the current page has a filter set.
@@ -38,7 +39,6 @@ class Asset_Manager {
 		}
 
 		// @todo Use plugin's version number.
-		wp_enqueue_style( 'wpshortlist', WPSHORTLIST_URL . '/css/style.css', array(), '1.0', 'all' );
 		wp_enqueue_script( 'wpshortlist', WPSHORTLIST_URL . '/js/wpshortlist.js', array( 'jquery' ), '1.0', true );
 
 		$data = array(
@@ -51,6 +51,21 @@ class Asset_Manager {
 		);
 		$code = 'const wpshortlistSettings = ' . wp_json_encode( $data );
 		wp_add_inline_script( 'wpshortlist', $code );
+	}
+
+	/**
+	 * Load styles.
+	 */
+	public function enqueue_styles() {
+		// Only load if the current page has a filter set.
+		$filter_sets = new Filter_Sets();
+		$start       = $filter_sets->get_start();
+		if ( ! $start ) {
+			return;
+		}
+
+		// @todo Use plugin's version number.
+		wp_enqueue_style( 'wpshortlist', WPSHORTLIST_URL . '/css/style.css', array(), '1.0', 'all' );
 	}
 
 }
